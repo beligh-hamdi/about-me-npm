15ed931542f4912154c224effc51d7071672d773
Beligh HAMDI <beligh.hamdi@gmail.com>
2024-04-03 16:49:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,33 +14,15 @@ const cardOptions = {
     margin: 1,
     borderStyle: "double",
     borderColor: "blue",
-    title: `${b("Sam Bellen")} - ${i("Principal Developer Advocate at Auth0")}`,
+    title: `${b("Beligh HAMDI")} - ${i("Principal Developer")}`,
     titleAlignment: 'center'
 };
 
 const content = `${b("Social links")}
 
-${g("Twitter")}            ${y("https://twitter.com/sambego")}
-${g("GitHub")}             ${y("https://github.com/sambego")}
-${g("Website")}            ${y("https://sambego.tech")}
-${g("LinkedIn")}           ${y("https://linkedin.com/in/sambellen/")}
-
-${b("Slides")}
-
-${g("JSON Web Tokens")}    ${y("https://jwt.sambego.tech")}
-${g("Passwordless")}       ${y("https://1990.sambego.tech")}
-${g("Webauthn")}           ${y("https://webauthn.sambego.tech")}
-${g("Web Cryptography")}   ${y("https://jose.sambego.tech")}
-${g("Access Control")}     ${y("https://ac.sambego.tech")}
-
-${g("Web Audio")}          ${y("https://audio.sambego.tech")}
-${g("Web APIs")}           ${y("https://go-web.sambego.tech")}
-${g("Web APIs")}           ${y("https://meow.sambego.tech")}
-
-${b("Experiments")}
-
-${g("RSA Playground")}      ${y("https://rsa.sambego.tech")}
-${g("DPOP Playground")}     ${y("https://dpop.sambego.tech")}`;
+${g("Twitter")}            ${y("https://twitter.com/belighpro")}
+${g("GitHub")}             ${y("https://github.com/beligh-hamdi")}
+${g("LinkedIn")}           ${y("https://linkedin.com/in/beligh")}
 
 const slidesContent = ``;
 
